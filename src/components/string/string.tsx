69d954c1a1b8bade0fb7f802d2d6fb56fb631072
vartuom <v.artuom@ya.ru>
2022-11-07 23:38:56 +0800

    reverse-string-done

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -4,17 +4,20 @@ import {Input} from "../ui/input/input";
 import {Button} from "../ui/button/button";
 import {Circle} from "../ui/circle/circle";
 import {ElementStates} from "../../types/element-states";
+import styles from "./string.module.css"
 
 export const StringComponent: React.FC = () => {
 
   const [value, setValue] = useState('');
   const [arr, setArr] = useState([] as Array<string>);
-  const [curr, setCurr] = useState(0);
+  const [currIndex, setCurrIndex] = useState(0);
+  const [isPending, setIsPending] = useState(false);
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
   };
 
+  //задержка рендеринга
   const delay = (ms: number) => new Promise<void>(
       resolve => setTimeout(resolve, ms)
   );
@@ -26,41 +29,48 @@ export const StringComponent: React.FC = () => {
     return arr
   }
 
+  //определяем цвет кружка на основе индекса следующего кандидата на перестановку
   const getState = (index: number) => {
-    if (index < curr || index > arr.length - 1 - curr) {
+    if (index < currIndex || index > arr.length - 1 - currIndex) {
       return ElementStates.Modified
     }
-    if (index === curr || index === arr.length - 1 - curr) {
+    if (index === currIndex || index === arr.length - 1 - currIndex) {
       return ElementStates.Changing
     }
     return ElementStates.Default
   }
 
   const reverseString = async () => {
+    //отключаем кнопку
+    setIsPending(true);
     let tempArr = value.split('');
+    setArr([...tempArr]);
+    setCurrIndex(0);
     for (let i = 0; i < Math.floor(tempArr.length / 2 ); i++) {
+      await delay(1000);
       let j = tempArr.length - 1 - i;
-      setCurr(i);
+      setCurrIndex(i+1);
       tempArr = swap(tempArr, i, j);
-      console.log(curr);
       setArr([...tempArr]);
-      await delay(500);
     }
+    //закрашиваем центр, если элементов было нечетное количество
+    setCurrIndex(Math.floor(tempArr.length / 2)+1);
+    setIsPending(false);
   }
 
 
   return (
     <SolutionLayout title="Строка">
-      <form onSubmit={e => {
+      <form className={styles.inputForm} onSubmit={e => {
         e.preventDefault()
         reverseString();
       }}>
         <Input value={value} onChange={onChange} placeholder={"Введите текст"} maxLength={11} isLimitText={true}/>
-        <Button type={"submit"} text={"Развернуть"}/>
+        <Button type={"submit"} text={"Развернуть"} isLoader={isPending}/>
       </form>
-      <div>
+      <div className={styles.circleRow}>
         {arr.map((char, index) => (
-            <Circle letter={char} state={getState(index)}/>
+            <Circle key={`${index}-${char}`} letter={char} state={getState(index)}/>
         ))}
       </div>
     </SolutionLayout>
