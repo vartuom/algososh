3206d732835acae943a82a2c2e724a13ff3c8ed5
vartuom <v.artuom@ya.ru>
2022-11-07 12:53:07 +0800

    init

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import {Input} from "../ui/input/input";
+import {Button} from "../ui/button/button";
 
 export const StringComponent: React.FC = () => {
+
+  const [value, setValue] = useState('');
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
   return (
     <SolutionLayout title="Строка">
-     
+    <form>
+      <Input value={value} onChange={onChange} placeholder={"Введите текст"} maxLength={11} isLimitText={true}/>
+      <Button type={"submit"} text={"Развернуть"}/>
+    </form>
     </SolutionLayout>
   );
 };
