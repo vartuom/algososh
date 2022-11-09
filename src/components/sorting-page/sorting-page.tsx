859868e7d634fdbb29f2fd4a59085f1de6365d7d
vartuom <93790667+vartuom@users.noreply.github.com>
2022-11-09 17:32:19 +0800

    09-11-work

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,10 +1,33 @@
-import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import React, {useState} from "react";
+import {SolutionLayout} from "../ui/solution-layout/solution-layout";
+import {RadioInput} from "../ui/radio-input/radio-input";
+import {Button} from "../ui/button/button";
+import styles from "./sorting-page.module.css"
+import {Direction} from "../../types/direction";
+import {ElementStates} from "../../types/element-states";
 
 export const SortingPage: React.FC = () => {
-  return (
-    <SolutionLayout title="Сортировка массива">
 
-    </SolutionLayout>
-  );
+    const [arr, setArr] = useState([] as any);
+
+    const generateRandomArray = () => {
+        const randomArr = Array.from(
+            {length: 5},
+            () => {return {value: Math.floor(Math.random()*101), state: ElementStates.Default}}
+        );
+        setArr([...randomArr])
+        console.log([...randomArr])
+    }
+
+    return (
+        <SolutionLayout title="Сортировка массива">
+            <form className={styles.inputForm}>
+                <RadioInput label={"Выбор"} />
+                <RadioInput label={"Пузырек"} />
+                <Button text={"По возрастанию"} sorting={Direction.Ascending}/>
+                <Button text={"По убыванию"} sorting={Direction.Descending}/>
+                <Button text={"Новый массив"} onClick={generateRandomArray}/>
+            </form>
+        </SolutionLayout>
+    );
 };
